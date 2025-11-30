635595a3667f31968914c662c87c34d1db58f3b9
wangchaochao <wangchaochao@58.com>
2025-11-30 17:28:53 +0800

    初始化springboot项目

---
+++ src/main/java/com/trae/UserManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.trae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementPlatformApplication.class, args);
+    }
+
+}
